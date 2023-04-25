edf29c25a80d456da65dfbde2782c1fb06c5bfd2
ockap <62844331+slewinus@users.noreply.github.com>
2023-04-25 15:08:01 +0200

    ex 03

--- C01/ex03/ft_div_mod.c
+++ C01/ex03/ft_div_mod.c
@@ -1,16 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_ultimate_ft.c                                   :+:      :+:    :+:   */
+/*   ft_div_mod.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ockap <ockap@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/25 11:23:49 by ockap             #+#    #+#             */
-/*   Updated: 2023/04/25 14:15:10 by ockap            ###   ########.fr       */
+/*   Created: 2023/04/25 14:42:17 by ockap             #+#    #+#             */
+/*   Updated: 2023/04/25 15:05:26 by ockap            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_ultimate_ft(int *********nbr)
+void	ft_div_mod(int a, int b, int *div, int *mod)
 {
-	*********nbr = 42;
+	*div = a / b;
+	*mod = a % b;
+}
+
+int	main(void)
+{
+	int		a;
+	int		b;
+	int		div;
+	int		mod;
+
+	a = 5;
+	b = 3;
+	ft_div_mod (a, b, &div, &mod);
+	return (0);
 }
